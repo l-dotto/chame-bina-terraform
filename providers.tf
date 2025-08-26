da2a8c6055d4d3e9a0d6f0497806cb3e2d9dd954
Premiersoft <luan.dotto@premiersoft.net>
2025-08-26 20:23:12 -0300

    Update providers and VPC

--- providers.tf
+++ providers.tf
@@ -1,9 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.10.0"
     }
+  }
+  backend "s3" {
+    bucket = "chame-bina"
+    key    = "terraform/eks/state/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
